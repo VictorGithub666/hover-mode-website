ae32a49414d715594649cb47a2591428d2b1eb88
VictorGithub666 <victorgithub666@gmail.com>
2025-05-28 09:51:03 +0300

    update hosting4

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import glsl from "vite-plugin-glsl";
 import path from "path";
 
 export default defineConfig({
+  base: "./",
+  build: {
+    outDir: "dist",
+  },
   plugins: [glsl()],
   assetsInclude: ["**/*.glb"], // Allow GLB imports
 });
